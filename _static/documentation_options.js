814bc2a1258e9f99c7056df3ac73336abc72c28d
djpugh <djpugh@users.noreply.github.com>
2020-12-19 01:39:15 +0000

    deploy: 3358cd7edcc0a3e2aed0670b63b4f37c048d05bc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.14',
+    VERSION: '0.1.15',
     LANGUAGE: 'English',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
