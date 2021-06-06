153a9e7bc2d0132d9823174fb7a04da13c14e948
David Pugh <djpugh@gmail.com>
2021-06-07 00:34:06 +0100

    Adding handling for token scopes in requests and alternate openapi auth flows.

--- src/fastapi_aad_auth/_base/validators/__init__.py
+++ src/fastapi_aad_auth/_base/validators/__init__.py
@@ -1,4 +1,4 @@
 
 from fastapi_aad_auth._base.validators.base import Validator  # noqa: F401
 from fastapi_aad_auth._base.validators.session import SessionValidator  # noqa: F401
-from fastapi_aad_auth._base.validators.token import TokenValidator  # noqa: F401
+from fastapi_aad_auth._base.validators.token import OAuthFlowType, TokenValidator  # noqa: F401
